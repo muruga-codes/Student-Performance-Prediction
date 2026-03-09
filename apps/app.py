f407268c92249ccffa6df2d2e9d0e382098db2c3
muruga-codes <sungodnika620@gmail.com>
2026-03-09 18:18:07 +0530

    initial change

--- apps/app.py
+++ apps/app.py
@@ -1,12 +1,11 @@
-
 import streamlit as st
 import pickle
+import pandas as pd
 
-# Load model
 with open("apps/model/student_model.pkl", "rb") as f:
     model = pickle.load(f)
 
-st.title("Student Performance Prediction")
+st.title("Student Performance Predictor")
 
 studytime = st.slider("Study Time", 1, 4)
 absences = st.slider("Absences", 0, 30)
@@ -14,5 +13,12 @@ g1 = st.slider("G1 Grade", 0, 20)
 g2 = st.slider("G2 Grade", 0, 20)
 
 if st.button("Predict"):
-    prediction = model.predict([[studytime, absences, g1, g2]])
+
+    input_data = pd.DataFrame(
+        [[studytime, absences, g1, g2]],
+        columns=["studytime","absences","G1","G2"]
+    )
+
+    prediction = model.predict(input_data)
+
     st.success(f"Predicted Final Grade: {prediction[0]:.2f}")
